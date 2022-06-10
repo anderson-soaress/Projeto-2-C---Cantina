75bf2b0761b324aa79ab334697fdcef21d5f2f29
Anderson-Soaress <88636005+Anderson-Soaress@users.noreply.github.com>
2022-06-10 09:46:35 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,41 @@
+
+
+
+public class Cliente {
+
+
+   //refazer essa parte,
+    private String nome;
+    private String pedido;
+    private int cod;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+
+
+
+
+    public String cc(){
+        System.out.println("NOME CLIENTE: ");
+
+        return nome;
+    }
+    public String pp(){
+        System.out.println("PEDIDO: ");
+
+        return pedido;
+    }
+
+    public int codig(){
+        System.out.println("CODIGO DO PEDIDO: ");
+        return cod;
+    }
+
+
+
+
+}
---
+++ MenuS.java
@@ -0,0 +1,22 @@
+
+
+public class MenuS {
+
+
+    private String me;
+
+
+
+    public void menuu(){
+
+        System.out.println("       MENU    ");
+        System.out.println("1.CRIAR NOVA LISTA");
+        System.out.println("2.ACESSAR LISTAS");
+        System.out.println("3.SAIR");
+
+    }
+
+    public void Codigo(int cod){
+
+    }
+}
