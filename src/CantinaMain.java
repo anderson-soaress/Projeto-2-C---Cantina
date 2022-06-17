353d31178335d30bbb330ede7a9899ec1a0a0aaf
Anderson-Soaress <88636005+Anderson-Soaress@users.noreply.github.com>
2022-06-17 17:46:53 -0300

    Update CantinaMain.java
    tem que melhorar

--- src/CantinaMain.java
+++ src/CantinaMain.java
@@ -8,9 +8,8 @@ public class CantinaMain {
     public static void main(String[] args) {
 
         //registra input
-        Scanner sc = new Scanner(System.in);
-        Scanner name = new Scanner(System.in);
-        Scanner pedido = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+
 
         //gera um codigo aleatorio, de 0 ate 200,
         Random rand = new Random();
@@ -22,35 +21,28 @@ public class CantinaMain {
             // CHAMA O MENU E LÊ O NUMERO DIGITADO
             MenuS mn = new MenuS();
             mn.menuu();
-            escolhaMenu = sc.nextInt();
+
+            escolhaMenu = input.nextInt();
 
             switch (escolhaMenu) {
 
                 // CRIA NOVA LISTA
+
                 case 1:
-                    Cliente cl = new Cliente();
-                    cl.cc();
-                    String nome = name.nextLine();
-
-                    cl.pp();
-                    String ped = pedido.nextLine();
-
-                    cl.codig();
-                    int int_random = rand.nextInt(limit);
-
-                    System.out.println(int_random);
-
-                    System.out.println("CLIENTE: " + nome + " PEDIDO: " + ped + " CODIGO: " + int_random);
-
+                    Cliente cliente1 = new Cliente();
+                    cliente1.cadastrarCliente();
+                    cliente1.mostrarCliente();
                     break;
 
                 // ACESSAR LISTAS
+
                 case 2:
                     System.out.println("");
 
                     break;
 
                 // ENCERRAR
+
                 case 3:
                     System.out.close();
 
@@ -64,3 +56,4 @@ public class CantinaMain {
         }
     }
 }
+
