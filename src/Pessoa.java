eb22df155b679e918b3c3f0ca44fdbded6828fbd
Anderson <88636005+Anderson-Soaress@users.noreply.github.com>
2022-06-18 15:34:56 -0300

    Add files via upload

---
+++ src/Pessoa.java
@@ -0,0 +1,45 @@
+public abstract class Pessoa {
+
+    // ATRIBUTOS
+
+    private String nome;
+    private int cpf;
+    private int telefone;
+    public boolean cadastrado;
+
+    // METODOS
+
+    // METODOS ESPECIAIS
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public boolean isCadastrado() {
+        return cadastrado;
+    }
+
+    public void setCadastrado(boolean cadastrado) {
+        this.cadastrado = cadastrado;
+    }
+}
