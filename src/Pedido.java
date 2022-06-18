7800388117012665fe519855ef8358164ece65b2
Anderson <88636005+Anderson-Soaress@users.noreply.github.com>
2022-06-18 15:45:33 -0300

    Add files via upload

---
+++ src/Pedido.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+
+public class Pedido {
+
+    // ATRIBUTOS
+
+    private String nome;
+    private int codigo;
+    private float preco;
+    private Cliente comprador;
+    private Funcionario vendedor;
+
+    // METODOS
+    
+
+    // METODOS ESPECIAIS
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public Cliente getComprador() {
+        return comprador;
+    }
+
+    public void setComprador(Cliente comprador) {
+        this.comprador = comprador;
+    }
+
+    public Funcionario getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(Funcionario vendedor) {
+        this.vendedor = vendedor;
+    }
+}
