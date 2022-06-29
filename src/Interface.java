7211ca4ba713c5547329f86f90295e8df1a5288a
Anderson <88636005+Anderson-Soaress@users.noreply.github.com>
2022-06-29 19:05:31 -0300

    Update and rename ContaBanco.java to Interface.java

---
+++ src/Interface.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Interface {
+
+
+    Scanner input = new Scanner(System.in);
+
+    public static void limpatela() {
+        for (int i = 0; i < 100; ++i)
+            System.out.println();
+    }
+}
